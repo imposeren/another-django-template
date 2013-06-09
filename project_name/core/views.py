aafcea9547d56b2cc419f20c64b62f4ec1e8a58d
Yaroslav Klyuyev <imposeren@gmail.com>
2013-06-09 18:00:18 +0300

    fixed typo

--- project_name/core/views.py
+++ project_name/core/views.py
@@ -5,5 +5,5 @@ from django.contrib import messages
 
 @render_to('core/index.html')
 def index(request):
-    messages.warning(request, 'This is default {{ project_name }}.core.views.index() view. Pleas override it')
+    messages.warning(request, 'This is default {{ project_name }}.core.views.index() view. Please override it')
     return {}
